2042a0fe92698493ef1fc41626a814c9ee34687b
Hongbusi <q951686662@qq.com>
2021-03-28 15:19:07 +0800

    404

--- layouts/404.vue
+++ layouts/404.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>404</div>
+  <div />
 </template>
+
+<script>
+export default {
+  mounted() {
+    this.initNoFoundPageByTencent();
+  },
+  methods: {
+    initNoFoundPageByTencent() {
+      const dom = document.createElement('script');
+      dom.setAttribute('homePageName', '回到首页');
+      dom.setAttribute('homePageUrl', '/');
+      dom.setAttribute('src', '//qzonestyle.gtimg.cn/qzone/hybrid/app/404/search_children.js');
+
+      document.body.append(dom);
+    }
+  }
+}
+</script>
