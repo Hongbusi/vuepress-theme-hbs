ab1dcfd35dd4b4be85f9efd0a4458fd9588c389b
Hongbusi <q951686662@qq.com>
2021-03-28 14:53:00 +0800

    plugins

--- example/.vuepress/config.js
+++ example/.vuepress/config.js
@@ -1,5 +1,21 @@
 module.exports = {
   title: 'Hello VuePress',
   description: 'Just playing around',
-  theme: require.resolve('../../')
+  theme: require.resolve('../../'),
+  themeConfig: {
+    nav: [
+      { text: 'blog', link: '/_posts/' },
+      { text: '时间轴', link: '/timeline/' }
+    ],
+    sidebar: {
+      collapsable: false,
+      '/_post/':   {
+        title: 'vuepress',
+        collapsable: false,
+        children: [
+          '/_post/'
+        ]
+      }
+    }
+  }
 }
--- layouts/Layout.vue
+++ layouts/Layout.vue
@@ -1,3 +1,18 @@
 <template>
-  <div>layout</div>
+  <div class="theme-layout">
+    <header>
+      <SearchBox />
+    </header>
+    <main>
+      <Content />
+    </main>
+  </div>
 </template>
+
+<script>
+import SearchBox from '@SearchBox';
+
+export default {
+  components: { SearchBox }
+}
+</script>
