f5f44aadb561e12b0765261e960ddb4d5c8d815e
Hongbusi <q951686662@qq.com>
2021-04-04 23:00:31 +0800

    404

--- layouts/404.vue
+++ layouts/404.vue
@@ -1,14 +1,35 @@
 <template>
-  <div />
+  <div class="theme-container">
+    <div class="theme-default-content">
+      <h1>404</h1>
+
+      <blockquote>{{ getMsg() }}</blockquote>
+
+      <RouterLink to="/">
+        Take me home.
+      </RouterLink>
+    </div>
+  </div>
 </template>
 
 <script>
+const msgs = [
+  `There's nothing here.`,
+  `How did we get here?`,
+  `That's a Four-Oh-Four.`,
+  `Looks like we've got some broken links.`
+];
+
 export default {
   mounted() {
     this.initNoFoundPageByTencent();
   },
 
   methods: {
+    getMsg () {
+      return msgs[Math.floor(Math.random() * msgs.length)]
+    },
+
     initNoFoundPageByTencent() {
       const dom = document.createElement('script');
       dom.setAttribute('homePageName', '回到首页');
