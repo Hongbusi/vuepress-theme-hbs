3b15321f2dba1b229a38d6efe7ffb54da777b0d8
Hongbusi <q951686662@qq.com>
2021-04-10 18:37:01 +0800

    update example

--- example/.vuepress/config.js
+++ example/.vuepress/config.js
@@ -2,5 +2,6 @@ module.exports = {
   title: 'vuepress-theme-hbs',
   description: 'A simple vuepress blog theme.',
   theme: require.resolve('../../packages'),
-  themeConfig: require('./config/themeConfig')
+  themeConfig: require('./config/themeConfig'),
+  plugins: require('./config/plugins')
 }
