83f9dc791fd31a6bf89f7dba28e51c474f3fb09a
Hongbusi <q951686662@qq.com>
2021-04-10 14:54:47 +0800

    update example

--- example/.vuepress/config.js
+++ example/.vuepress/config.js
@@ -2,13 +2,5 @@ module.exports = {
   title: 'vuepress-theme-hbs',
   description: 'A simple vuepress blog theme.',
   theme: require.resolve('../../packages'),
-  themeConfig: {
-    // 作者
-    author: 'Hongbusi',
-    // 备案号
-    record: '浙ICP备2020035758号-2',
-    recordLink: 'https://beian.miit.gov.cn',
-    // 项目开始时间
-    startYear: '2021'
-  }
+  themeConfig: require('./config/themeConfig')
 }
---
+++ example/.vuepress/config/themeConfig/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // 作者
+  author: 'Hongbusi',
+  // 备案号
+  record: '浙ICP备2020035758号-2',
+  recordLink: 'https://beian.miit.gov.cn',
+  // 项目开始时间
+  startYear: '2021'
+}
