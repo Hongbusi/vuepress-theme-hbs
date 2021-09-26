4d8facfa8e60bb252960adc5b7aeb1d910ad6dd7
Hongbusi <q951686662@qq.com>
2021-09-26 14:04:01 +0800

    init vuepress-theme-hbs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,6 @@ const { path } = require('@vuepress/utils');
 
 module.exports = {
   title: 'vuepress-theme-hbs',
-  description: '桃李不言，下自成蹊！',
   theme: path.resolve(__dirname, '../../packages/vuepress-theme-hbs'),
   themeConfig: {
   }
