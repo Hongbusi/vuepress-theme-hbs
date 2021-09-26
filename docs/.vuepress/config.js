3284854f7495146b7ee0331ecee9f90aaf35c66b
Hongbusi <q951686662@qq.com>
2021-09-26 11:14:16 +0800

    init vuepress-theme-hbs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+const { path } = require('@vuepress/utils');
+
+module.exports = {
+  title: 'vuepress-theme-hbs',
+  description: '桃李不言，下自成蹊！',
+  theme: path.resolve(__dirname, '../../packages/vuepress-theme-hbs'),
+  themeConfig: {
+  }
+}
---
+++ packages/vuepress-theme-hbs/layouts/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <Layout>
+    <template #navbar>
+      <header class="navbar">This is my custom navbar.</header>
+    </template>
+  </Layout>
+</template>
+
+<script>
+import Layout from '@vuepress/theme-default/lib/client/layouts/Layout.vue';
+
+export default {
+  components: {
+    Layout
+  }
+}
+</script>
