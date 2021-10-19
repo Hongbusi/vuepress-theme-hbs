e1b3d246cf088d3272f1c4a84893add0a2411a06
Hongbusi <q951686662@qq.com>
2021-10-19 18:27:21 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ const { path } = require('@vuepress/utils');
 
 module.exports = {
   title: 'vuepress-theme-hbs',
-  theme: path.resolve(__dirname, '../../packages/vuepress-theme-hbs'),
+  theme: path.resolve(__dirname, '../../packages'),
   themeConfig: {
   }
 }
