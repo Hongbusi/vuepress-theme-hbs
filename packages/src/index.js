8df49532f9aaee9c6ac5e09168b13b34efa4b0ac
Hongbusi <q951686662@qq.com>
2021-10-19 18:33:45 +0800

    update

--- packages/src/index.js
+++ packages/src/index.js
@@ -1,9 +1,11 @@
 const { path } = require('@vuepress/utils');
 
-module.exports = {
+const hbsTheme = {
   name: 'vuepress-theme-hbs',
   layouts: {
     Layout: path.resolve(__dirname, 'layouts/Layout.vue'),
     404: path.resolve(__dirname, 'layouts/404.vue')
   }
 }
+
+module.exports = hbsTheme;
