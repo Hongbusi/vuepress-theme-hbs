1ecc09d4b6cbc5aba448e9d93b368bc8fff25586
Hongbusi <q951686662@qq.com>
2021-10-26 10:52:24 +0800

    lerna

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ const { path } = require('@vuepress/utils');
 
 module.exports = {
   title: 'vuepress-theme-hbs',
-  theme: path.resolve(__dirname, '../../packages'),
+  theme: path.resolve(__dirname, '../../packages/vuepress-theme-hbs'),
   themeConfig: {
     navbar: [
       { text: '首页', link: '/' },
---
+++ packages/vuepress-theme-hbs/__tests__/vuepress-theme-hbs.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const vuepressThemeHbs = require('..');
+
+describe('vuepress-theme-hbs', () => {
+    it('needs tests');
+});
